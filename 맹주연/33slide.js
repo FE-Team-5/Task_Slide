c4d74767251c9914cd37d1dcbb3376372fe67a52
jymaeng <jymaeng1234@gmail.com>
2024-08-03 20:30:17 +0900

    [ADD] html,css,js 추가

---
+++ 맹주연/33slide.js
@@ -0,0 +1,8 @@
+const $sliderUl = document.querySelector(".sliderUl");
+
+const $prevBtn = document.querySelector(".prev");
+const $nextBtn = document.querySelector(".next");
+
+const $liArr = $sliderUl.querySelectorAll(li);
+
+$liArr.forEach();
