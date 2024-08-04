131b994bf63454d09ded22f1e50979d0ec6954d4
JW-Ahn0 <jun.woo.an0@gmail.com>
2024-08-05 04:47:45 +0900

    [Feat] dot pagination 추가

--- 안준우/slide.js
+++ 안준우/slide.js
@@ -1,30 +1,46 @@
+const $sliderUI = document.querySelector(".sliderUl");
 const slideList = document.querySelectorAll(".slider");
+const $dotContainer = document.querySelector(".slider-dot");
+const dotList = $dotContainer.querySelectorAll("span");
+
 let currentInx = 0;
 let onClickDelayFlag = false;
+let isTransitioning = false; // Avoid transition conflicts
+const transitionDuration = 0; // ms
+
+function moveSlideByIdx(index) {
+  /*
+    해당 인덱스 위치로 이동하는 함수
+    */
+}
 
 function slideInit() {
-  const $sliderUI = document.querySelector(".sliderUl");
   const cloneLastItem = slideList[slideList.length - 1].cloneNode(true);
   slideList[slideList.length - 1].remove();
+  slideList[slideList.length - 1] = cloneLastItem;
   $sliderUI.prepend(cloneLastItem);
 }
 
-function moveSlide(direction) {
+function moveSlide(direction, milliseconds) {
   if (onClickDelayFlag) return;
   onClickDelayFlag = true;
 
-  const $sliderUI = document.querySelector(".sliderUl");
+  const setTimeOutDelayTime = milliseconds;
+  const transitionDelayTime = milliseconds / 1000 + "s";
   const currentSlideList = document.querySelectorAll(".slider");
 
   let transformValue;
   if (direction === "next") {
     transformValue = "-66.6666%";
+    currentInx = currentInx + 1 > slideList.length - 1 ? 0 : currentInx + 1;
   } else {
     transformValue = "0";
+    currentInx = currentInx - 1 < 0 ? slideList.length - 1 : currentInx - 1;
   }
 
-  $sliderUI.style.transition = "0.5s";
+  $sliderUI.style.transition = transitionDelayTime;
   $sliderUI.style.transform = `translateX(${transformValue})`;
+  moveDot(currentInx);
 
   setTimeout(() => {
     $sliderUI.style.transition = "none";
@@ -32,24 +48,41 @@ function moveSlide(direction) {
       const cloneNode = currentSlideList[0].cloneNode(true);
       currentSlideList[0].remove();
       $sliderUI.append(cloneNode);
-      currentInx = currentInx + 1 > slideList.length - 1 ? 0 : currentInx + 1;
     } else {
       const cloneNode =
         currentSlideList[currentSlideList.length - 1].cloneNode(true);
       currentSlideList[currentSlideList.length - 1].remove();
       $sliderUI.prepend(cloneNode);
-      currentInx = currentInx - 1 < 0 ? slideList.length - 1 : currentInx - 1;
     }
     $sliderUI.style.transform = "translateX(-33.3333%)";
     onClickDelayFlag = false;
-  }, 500);
+  }, setTimeOutDelayTime);
+}
+
+function moveDot(index) {
+  dotList.forEach((element, i) => {
+    if (index === i) {
+      element.style.backgroundColor = "black";
+      element.style.color = "white";
+    } else {
+      element.style.backgroundColor = "tomato";
+      element.style.color = "black";
+    }
+  });
+}
+function onDotClick(e, index) {
+  moveSlideByIdx(index);
 }
 
 function initEvent() {
   const $nextBtn = document.querySelector(".next");
   const $prevBtn = document.querySelector(".prev");
-  $nextBtn.addEventListener("click", () => moveSlide("next"));
-  $prevBtn.addEventListener("click", () => moveSlide("prev"));
+  $nextBtn.addEventListener("click", () => moveSlide("next", 500));
+  $prevBtn.addEventListener("click", () => moveSlide("prev", 500));
+  dotList.forEach((element, index) => {
+    element.addEventListener("click", (e) => onDotClick(e, index));
+  });
 }
+
 slideInit();
 initEvent();
